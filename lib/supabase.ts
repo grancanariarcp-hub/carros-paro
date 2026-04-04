a9886f17ecdeff40733a25e640bcb9202eae7600
grancanariarcp-hub <grancanariarcp@gmail.com>
2026-04-04 19:16:38 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,3 +6,16 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
+
+export function createPublicClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+      }
+    }
+  )
+}
